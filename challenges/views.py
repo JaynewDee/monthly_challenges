cc3d59b739f56175848eaf3ef3faf5bce0987fda
NuminousBlue <jdiehl2236@gmail.com>
2022-01-02 17:38:47 -0600

    reworking views functions

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-     return HttpResponse("Here is your basic response!")
+def monthly_challenge(request, month):
+     challenge_text = None
+     if month == 'january':
+          challenge_text = 'Take a shower and get some exercise, ya filthy animal'
+     elif month == 'february':
+          challenge_text = "It's february in Texas, where nothing breaks easy."
+          
 
