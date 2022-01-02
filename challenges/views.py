a3eedb2e0458a572139dcfef8bd4f1fa95dff995
NuminousBlue <jdiehl2236@gmail.com>
2022-01-02 17:59:46 -0600

    attempting to dry up views code

--- challenges/views.py
+++ challenges/views.py
@@ -9,5 +9,8 @@ def monthly_challenge(request, month):
           challenge_text = 'Take a shower and get some exercise, ya filthy animal'
      elif month == 'february':
           challenge_text = "It's february in Texas, where nothing breaks easy."
+     else:
+          return HttpResponseNotFound('Slow down there, mate, lets survive the month')
+     return HttpResponse(challenge_text)
           
 
